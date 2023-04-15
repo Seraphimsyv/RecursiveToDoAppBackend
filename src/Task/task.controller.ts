b137b32fa8c86be3cb944b6a08dc2eeb0e2ebe74
Seraphimsyv <oshinokelen@gmail.com>
2023-04-15 17:23:46 +0300

    Update task controller and service

--- src/Task/task.controller.ts
+++ src/Task/task.controller.ts
@@ -15,4 +15,19 @@ export class TaskController {
   findChilds(@Param("id") id : number) : Promise<Task[]> {
     return this.taskService.findChilds(id);
   }
+
+  @Post()
+  async create(@Body() task: Task) : Promise<Task> {
+    return this.taskService.create(task);
+  }
+
+  @Post()
+  async swapp(@Body() id: number, moveTo: "up" | "down") : Promise<void> {
+    return this.taskService.swappingTask(id, moveTo);
+  }
+
+  @Delete(":id")
+  async delete(@Param("id") id: number) : Promise<void> {
+    return this.taskService.remove(id);
+  }
 }
--- src/Task/task.service.ts
+++ src/Task/task.service.ts
@@ -15,7 +15,34 @@ export class TaskService {
   }
 
   async findChilds(id: number) : Promise<Task[]> {
-    return this.taskRepository.find();
+    const tasks = await this.taskRepository.find({
+      where: { parentId: id },
+    });
+    return tasks;
+  }
+
+  async create(task: Task) : Promise<Task> {
+    return this.taskRepository.save(task);
+  }
+
+  async swappingTask( id: number, moveTo: "up" | "down" ) : Promise<void> {
+    const firstTask = await this.taskRepository.findOne({
+      where: { id: id },
+    });
+    const secondTask = await this.taskRepository.findOne({
+      where: {
+        parentId: firstTask.parentId,
+        place: moveTo === "up" ? firstTask.place - 1 : firstTask.place + 1
+      },
+    });
+    firstTask.place = moveTo === "up" ? firstTask.place-1 : firstTask.place+1;
+    secondTask.place = moveTo === "up" ? secondTask.place+1 : secondTask.place-1;
+    await this.taskRepository.save(firstTask);
+    await this.taskRepository.save(secondTask);
+  }
+
+  async remove(id: number) : Promise<void> {
+    await this.taskRepository.delete(id);
   }
 }
 
